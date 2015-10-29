c130766a5effb9b83bc614a08837009d6ae6226b
阿南 <wangyn@59store.com>
2015-10-29 15:29:54 +0800

    Add a mapping 3 to 4.

--- TestCoreData/TestCoreData/CoreData/Department+CoreDataProperties.swift
+++ TestCoreData/TestCoreData/CoreData/Department+CoreDataProperties.swift
@@ -19,3 +19,29 @@ extension Department {
     @NSManaged var manager: Manager?
 
 }
+
+
+class DepartmentToDepartmentPolicy: NSEntityMigrationPolicy {
+    
+    override func createDestinationInstancesForSourceInstance(sInstance: NSManagedObject, entityMapping mapping: NSEntityMapping, manager: NSMigrationManager) throws {
+        
+        let userInfo:NSDictionary = mapping.userInfo!
+        let modelVersion = userInfo.valueForKey("modelVersion")
+        
+        if 4 == modelVersion?.integerValue {
+            let newObject = NSEntityDescription.insertNewObjectForEntityForName(mapping.destinationEntityName!, inManagedObjectContext: manager.destinationContext)
+            
+            let oldNumber = sInstance.valueForKey("number")
+            let oldNumberStr = oldNumber?.string
+            
+            newObject.setValue(oldNumberStr, forKey: "number")
+            
+            manager.associateSourceInstance(sInstance, withDestinationInstance: newObject, forEntityMapping: mapping)
+        } else {
+            try
+            super.createDestinationInstancesForSourceInstance(sInstance, entityMapping: mapping, manager: manager)
+        }
+        
+        
+    }
+}
